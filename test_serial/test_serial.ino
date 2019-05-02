946891e66ff0958b003725e7dff9b29aff3f1f37
Torrmai <31347855+Torrmai@users.noreply.github.com>
2019-05-02 22:04:10 +0700

    Add demo for serial comunication
    I think it will more suitable to use struct data type in C or C++ if your want to send more than one data

---
+++ test_serial/test_serial.ino
@@ -0,0 +1,29 @@
+//for Arduino UNO board(display unit)
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+const int nCols = 16;
+const int nRows = 2;
+//Setting up the lcd 
+int tmp;
+LiquidCrystal_I2C lcd(0x27, nCols,nRows);
+void setup() {
+  lcd.begin();
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(Serial.available() > 0)
+  {
+    lcd.clear(); //clear error when don't connect properly
+    tmp = Serial.read();
+    lcd.write(tmp);
+    delay(2000);//have to delay == Practicum
+    lcd.clear();
+  }
+  else{
+    lcd.print("Connetion!!");//for error exception in lcd
+    while(Serial.available() == 0)
+    {}
+  }
+}
---
+++ test_serial_for_practicum/test_serial_for_practicum.ino
@@ -0,0 +1,10 @@
+//test counting number
+void setup() {
+  Serial.begin(9600);
+}
+int i = 0;
+void loop() {
+  Serial.print(i%10);
+  delay(2000);
+  i++;
+} 

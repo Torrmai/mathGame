5dacf252b86896aa78e068406a38c98ff5e4c1d7
Torrmai <coverleaf@localhost.localdomain>
2019-05-05 11:56:23 +0700

    There are some bugs when inputing a number

--- main_alpha0.01/main_alpha0.01.ino
+++ main_alpha0.01/main_alpha0.01.ino
@@ -3,11 +3,20 @@
  * เนื่องจาก lcd ยังอยู่กับอีกคนฉะนั้นจะใช้การเขียนเท่าที่จะทำได้ก่อนละกัน
  * ปล.คิดว่าน่าจะต้องเปลี่ยนอะไรไม่มากในส่วนการแสดงผลมั้ง
 */
-#define SEC 1000
+#define SEC 1000 // millis second sometime is hard to read so let define it thousand millis sec =  1 sec to make life more easier 
 #include <pt.h>
 static struct pt pt_inp,pt_count_down;
 bool time_out =  false,preesed = false;
-int digit = 0;int tmp = 0;
+int digit = 0;int tmp = 0;int rd_inp = 0;
+int pw(int a,int b)
+{
+  int rtn = 1;
+  for(int i=0;i<b;i++)
+  {
+    rtn *= a;
+  }
+  return rtn;
+}
 static int countDown(struct pt *pt)
 {
   static unsigned long t = 0;
@@ -17,9 +26,12 @@ static int countDown(struct pt *pt)
     t = millis();
     PT_WAIT_UNTIL(pt,millis() - t > 1*SEC);
     tmp += 1;
-    if(tmp == 10)
+    if(tmp == 10)//อยากให้TIMEOUTเป็นกี่วิก็เปลี่ยนตรงนี้นะฮะ
     {
+      Serial.println("TIME OUT!!!!");
       tmp = 0;
+      rd_inp =  0;
+      digit = 0;
       time_out = true;     
     }
     else time_out = false;
@@ -45,12 +57,16 @@ static int inp(struct pt *pt)
         // rd == 'A'
         if(rd == 17)
         {
-          tmp = 0;
+          tmp = 0;//reset time to 0 when answer
+          rd_inp = 0;//ยังไม่ได้รวมกับตอน Random ตัวเกม
+          digit = 0;//ยังไม่ได้รวมกับตอน Random ตัวเกม
         }
         else{
           Serial.print(tmp);
           Serial.print(" :t-> ");
-          Serial.println(rd);
+          rd_inp += rd*pw(10,digit);
+          Serial.println(rd_inp);
+          digit++;
         }
       }
     }
